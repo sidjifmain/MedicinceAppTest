329317c05d30627083a3a5ddd4692ef6217d21d3
sidjif <sidjifmain@mail.com>
2023-10-21 21:31:26 +0400

    First Commit

---
+++ app/src/main/java/com/example/medicineview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.medicineview
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.black)
+        }
+    }
+}
